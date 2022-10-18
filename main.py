cc4b2264f8e223a54dc39528d67e0c7a6c44e2a6
aniket-607 <93555409+aniket-607@users.noreply.github.com>
2022-10-18 10:21:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # pip install streamlit fbprophet yfinance plotly
+from turtle import color
 import streamlit as st
 from datetime import date
-
+from PIL import Image
 import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
@@ -10,13 +11,16 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Stock Forecast App')
+image = Image.open('logo.png')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME','BTC-USD')
+st.image(image, width = 150)
+st.title('Crypto Forecast App')
+
+stocks = ('BTC-USD','ETH-USD','BNB-USD','SOL-USD','DOGE-USD','DOT-USD')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+n_Months= st.slider('Months of prediction:', 1, 12)
+period = n_Months*30
 
 
 def load_data(ticker):
@@ -56,7 +60,7 @@ forecast = m.predict(future)
 st.subheader('Forecast data')
 st.write(forecast.tail())
     
-st.write(f'Forecast plot for {n_years} years')
+st.write(f'Forecast plot for {n_Months} Months')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
